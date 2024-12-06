47de928d5dc5a1d17089f38cc8312a9165bcb26e
rosenrose <crazytempler@gmail.com>
2024-12-06 16:57:17 +0900

    function

--- main.dart
+++ main.dart
@@ -2,35 +2,36 @@ import "dart:io";
 import "dart:convert";
 
 void main() {
-  var score = "zz";
-  var superpower = 123;
-  var player = {
-    "name": "nico",
-    score.toUpperCase(): 19.99,
-    superpower + 3: false,
-    "": 1,
-    null: 2,
-    "name": "lynn"
-  };
+  sayHello("foo");
+  print(getHello("bar"));
+  print(add(3, 4));
+  print("");
 
-  print(player);
+  sayNamedParam(name: "nico", country: "Columbia", age: 30);
+  sayNamedParam(name: "admin");
+  //sayNamedParam(country: "Korea");
+  //sayNamedParam();
+  print("");
 
-  var nums = {
-    3,
-    3,
-    3,
-    1,
-    2,
-    2,
-    [1, 2],
-    [1, 2],
-    null,
-    null,
-    "abc",
-    "abc",
-    true,
-    true
-  };
+  sayOptionalPositionalParam("kjw4569", 100);
+}
 
-  print(nums);
+void sayHello(String name) {
+  print("Hello $name!");
+}
+
+String getHello(String name) {
+  return "Hi $name";
+}
+
+num add(num a, num b) => a + b;
+
+void sayNamedParam(
+    {required String name, int age = -1, String country = "default"}) {
+  print("$name(age: $age) from $country");
+}
+
+void sayOptionalPositionalParam(String name, int age,
+    [String? country = "country"]) {
+  print("$name(age: $age) from $country");
 }
