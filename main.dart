90473382487600eb278a15fdcaaa42e3bd5be7cc
rosenrose <crazytempler@gmail.com>
2024-12-06 19:21:44 +0900

    abstract class & inheritance & mixin

--- main.dart
+++ main.dart
@@ -4,40 +4,62 @@ import "dart:typed_data";
 
 enum Team { red, blue, green, black }
 
-class Player {
-  String name;
+abstract class Animal {
+  void walk();
+}
+
+class Human extends Animal {
+  final String name;
+
+  Human({required this.name});
+
+  void walk() {
+    print("Human walks");
+  }
+
+  void say() {
+    print("name is $name.");
+  }
+}
+
+mixin Power {
+  final double power = 150.99;
+}
+
+mixin Runner {
+  void run() {
+    print("Run!");
+  }
+}
+
+class Player extends Human with Power, Runner {
   num hp;
   Team team;
 
   Player({
-    required this.name,
+    required String name,
     required this.hp,
     required this.team,
-  });
+  }) : super(name: name);
 
-  Player.createBluePlayer({required String name})
-      : this.name = name,
-        this.hp = 100,
-        this.team = Team.blue;
-
-  void setName(name) {
-    this.name = name ?? "";
+  @override
+  void say() {
+    super.say();
+    print("team: $team");
   }
 
   String getStatus() => "${[name, hp, team].join(", ")}";
 }
 
 void main() {
-  var p1 = Player(name: "foo", hp: 200, team: Team.red)
-    ..name = "bar"
-    ..hp = 999
-    ..team = Team.green;
-  print(p1.getStatus());
+  var p1 = Player(
+    name: "foo",
+    hp: 200,
+    team: Team.red,
+  );
 
-  var p2 = p1
-    ..team = Team.black
-    ..hp = -1
-    ..setName("wow")
-    ..getStatus();
-  print(p2.getStatus());
+  p1.walk();
+  p1.say();
+  print(p1.power);
+  p1.run();
 }
