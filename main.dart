a483f52a04932b20ad5391cb8ad71bce7d873f07
rosenrose <crazytempler@gmail.com>
2024-12-06 18:48:03 +0900

    cascade notation & enum

--- main.dart
+++ main.dart
@@ -2,77 +2,42 @@ import "dart:io";
 import "dart:convert";
 import "dart:typed_data";
 
-int seq = 0;
+enum Team { red, blue, green, black }
 
 class Player {
-  late final int id;
   String name;
   num hp;
-  String team;
-  int age;
+  Team team;
 
   Player({
     required this.name,
     required this.hp,
     required this.team,
-    required this.age,
-  }) {
-    this.id = seq;
-    seq++;
-  }
+  });
 
-  Player.createBluePlayer({required String name, required int age})
-      : this.id = seq++,
-        this.name = name,
-        this.age = age,
+  Player.createBluePlayer({required String name})
+      : this.name = name,
         this.hp = 100,
-        this.team = "blue";
-
-  Player.fromJson(Map<String, dynamic> json)
-      : this.id = seq++,
-        this.name = json["name"],
-        this.team = json["team"],
-        this.hp = json["hp"],
-        this.age = 0;
+        this.team = Team.blue;
 
   void setName(name) {
     this.name = name ?? "";
   }
 
-  String getStatus() => "($id) ${[name, hp, team, age].join(", ")}";
+  String getStatus() => "${[name, hp, team].join(", ")}";
 }
 
 void main() {
-  var b1 = Player.createBluePlayer(name: "nico", age: -1);
-  print(b1.getStatus());
+  var p1 = Player(name: "foo", hp: 200, team: Team.red)
+    ..name = "bar"
+    ..hp = 999
+    ..team = Team.green;
+  print(p1.getStatus());
 
-  var b2 = Player.createBluePlayer(name: "foo", age: 234);
-  print(b2.getStatus());
-  print("");
-
-  var apiData = [
-    {
-      "name": "franch122",
-      "team": "red",
-      "hp": 0,
-    },
-    {
-      "name": "lynn",
-      "team": "blue",
-      "hp": 10,
-    },
-    {
-      "name": "bar",
-      "team": "red",
-      "hp": 474,
-    },
-  ];
-
-  for (final json in apiData) {
-    print(Player.fromJson(json).getStatus());
-  }
-
-  apiData.forEach((json) {
-    print(Player.fromJson(json).getStatus());
-  });
+  var p2 = p1
+    ..team = Team.black
+    ..hp = -1
+    ..setName("wow")
+    ..getStatus();
+  print(p2.getStatus());
 }
