9aabae28576a7946a46edb606cb3852db1909b79
rosenrose <crazytempler@gmail.com>
2024-12-06 18:36:46 +0900

    named constructor

--- main.dart
+++ main.dart
@@ -7,30 +7,72 @@ int seq = 0;
 class Player {
   late final int id;
   String name;
-  int hp;
+  num hp;
+  String team;
+  int age;
 
-  Player(this.name, this.hp) {
+  Player({
+    required this.name,
+    required this.hp,
+    required this.team,
+    required this.age,
+  }) {
     this.id = seq;
     seq++;
   }
+
+  Player.createBluePlayer({required String name, required int age})
+      : this.id = seq++,
+        this.name = name,
+        this.age = age,
+        this.hp = 100,
+        this.team = "blue";
+
+  Player.fromJson(Map<String, dynamic> json)
+      : this.id = seq++,
+        this.name = json["name"],
+        this.team = json["team"],
+        this.hp = json["hp"],
+        this.age = 0;
 
   void setName(name) {
     this.name = name ?? "";
   }
 
-  String getStatus() => "($id) $name: $hp";
+  String getStatus() => "($id) ${[name, hp, team, age].join(", ")}";
 }
 
 void main() {
-  var p1 = Player("kjw4569", 50);
-  print(p1);
-  print(p1.name);
+  var b1 = Player.createBluePlayer(name: "nico", age: -1);
+  print(b1.getStatus());
 
-  // p1.hp = 417;
-  // print(p1.hp);
-  p1.setName("foo");
-  print(p1.getStatus());
+  var b2 = Player.createBluePlayer(name: "foo", age: 234);
+  print(b2.getStatus());
+  print("");
 
-  var p2 = Player("bar", 100);
-  print(p2.getStatus());
+  var apiData = [
+    {
+      "name": "franch122",
+      "team": "red",
+      "hp": 0,
+    },
+    {
+      "name": "lynn",
+      "team": "blue",
+      "hp": 10,
+    },
+    {
+      "name": "bar",
+      "team": "red",
+      "hp": 474,
+    },
+  ];
+
+  for (final json in apiData) {
+    print(Player.fromJson(json).getStatus());
+  }
+
+  apiData.forEach((json) {
+    print(Player.fromJson(json).getStatus());
+  });
 }
